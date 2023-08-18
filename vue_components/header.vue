6c8d7d5d6b30d06547a05be879ab4abe7e6a43a2
thaind <datpt7@rikkeisoft.com>
2023-08-18 11:37:56 +0700

    first

---
+++ vue_components/header.vue
@@ -0,0 +1,55 @@
+<template>
+	<header>
+		<div class="container">
+			<div class="menu">
+				<div class="menu-hamburger">
+					<div class="menu-hamburger-line"></div>
+					<div class="menu-hamburger-line"></div>
+					<div class="menu-hamburger-line"></div>
+				</div>
+				<div class="menu-overlay"></div>
+				<div class="menu-items">
+					<div class="menu-links">
+						<div class="menu-parent">
+							<a href="" title="">Trang chủ</a>
+						</div>
+					</div>
+					<div class="menu-links">
+						<div class="menu-parent">
+							<a href="" title="">Giới thiệu</a>
+						</div>
+					</div>
+					<div class="menu-links">
+						<div class="menu-parent">
+							<a href="" title="">Công trình</a>
+							<i class="fa fa-chevron-down" aria-hidden="true"></i>
+						</div>
+						<div class="menu-sub">
+							<div class="menu-sub-items">
+								<a href="" title="">Chung cư</a>
+								<a href="" title="">Biệt Thự</a>
+								<a href="" title="">Nhà Phố</a>
+								<a href="" title="">Cửa Hàng</a>
+							</div>
+						</div>
+					</div>
+					<div class="menu-links">
+						<div class="menu-parent">
+							<a href="" title="">Tin tức</a>
+						</div>
+					</div>
+					<div class="menu-links">
+						<div class="menu-parent">
+							<a href="" title="">Tuyển dụng</a>
+						</div>
+					</div>
+					<div class="menu-links">
+						<div class="menu-parent">
+							<a href="" title="">Liên hệ</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
